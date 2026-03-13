59a7dc60f98f999a27aff2ad8bf31581b1b98684
nasya1124 <nnaz0017@student.monash.edu>
2026-03-14 02:31:25 +1100

    Initial Xcode project setup

---
+++ WhatsLeft/WhatsLeft/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsLeft
+//
+//  Created by Nazara  on 14/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsLeft/WhatsLeft/WhatsLeftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsLeftApp.swift
+//  WhatsLeft
+//
+//  Created by Nazara  on 14/03/26.
+//
+
+import SwiftUI
+
+@main
+struct WhatsLeftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
